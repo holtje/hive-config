37a96d27c26e5d314686682bd08ec97efd6f564d
Christian Höltje <docwhat@gerf.org>
2020-10-28 22:28:52 -0400

    chore: update blog image

---
+++ script/update-image-from-ghcr.bash
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+set -Eeuo pipefail
+
+# This only works because currently, ghcr.io keeps the tag list
+# in the order created.
+
+get_latest_tag() {
+  local -r name=$1
+  curl \
+    -sS \
+    -X GET \
+    -H "Accept: application/vnd.docker.distribution.manifest.v2+json" \
+    -H "Authorization: token ${GITHUB_TOKEN}" \
+    "https://ghcr.io/v2/${name}/tags/list" |
+    jq -r '([.tags[] | select(startswith("sha-"))])[-1]'
+}
+
+write_tag() {
+  local -r tag=$1
+  local -r file=$2
+
+  yq write \
+    --inplace "$file" \
+    'spec.template.spec.containers[0].image' \
+    "ghcr.io/docwhat/blog:${tag}"
+}
+
+write_tag "$(get_latest_tag docwhat/blog)" docwhat-manifests/blog-staging.yaml
+
+# EOF
